4bfbb44cfe089fab2bc70e1f6ed69bdf450bf833
Jstickz <adeoyejohn15@gmail.com>
2024-11-25 04:24:24 +0100

    Third Commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const submit = document.getElementById("submit");
 
 let userRating;
 
-rating.forEach((button) => [
+rating.forEach((button) => {
   button.addEventListener("click", (e) => {
     rating.forEach((btn) => {
       btn.classList.remove("selected");
@@ -11,15 +11,14 @@ rating.forEach((button) => [
 
     button.classList.add("selected");
     userRating = e.target.value;
-  }),
-]);
+  });
+});
 
 submit.addEventListener("click", () => {
   if (!userRating) {
     alert("You need to select a rating");
   } else {
     window.location.href = "./thank-you.html";
-
     window.localStorage.setItem("user-rating", userRating);
   }
 });
